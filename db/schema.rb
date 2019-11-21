52dda5598f8f11c5bbf1271eaf1488721c34f0b5
LimitToInfinity <21stLimitless@gmail.com>
2019-11-21 11:42:49 -0700

    Changed gem and database.yml all to postgresql from sqlite3.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_11_134242) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "crusts", force: :cascade do |t|
     t.string "dough"
     t.decimal "price"
